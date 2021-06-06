da35df0d9f99c936edc4867dd85adcc243490830
Pun Wing Yin <patrickwypun04@gmail.com>
2021-06-06 08:16:01 +0000

    fixing some timezone issue

--- src/components/result_panel.vue
+++ src/components/result_panel.vue
@@ -2,14 +2,24 @@
     <v-sheet color="white" class="overflow-y-auto overflow-x-hidden">
         <v-container class="pa-3" fluid>
             <v-row>
-                <v-col class="pa-1 ma-0" v-for="(item, index) in displayed_list" :key="index" cols='12' sm='6' md='4' lg='3' xl='3'>
+                <v-col
+                    class="pa-1 ma-0"
+                    v-for="(item, index) in displayed_list"
+                    :key="index"
+                    cols="12"
+                    sm="6"
+                    md="4"
+                    lg="3"
+                    xl="3"
+                >
                     <v-lazy
                         :options="{
-                            threshold: .1
+                            threshold: 0.1,
                         }"
                         min-height="250"
-                        transition="fade-transition">
-                        <program_card :program="item"/>
+                        transition="fade-transition"
+                    >
+                        <program_card :program="item" />
                     </v-lazy>
                 </v-col>
             </v-row>
@@ -19,13 +29,12 @@
 
 <script>
 export default {
-    name: 'result_panel',
-    props: ['displayed_list'],
+    name: "result_panel",
+    props: ["displayed_list"],
     components: {
-        program_card: () => import('./program_card')
-    }
-}
+        program_card: () => import("./program_card"),
+    },
+};
 </script>
 
-<style lang="css" scoped>
-</style>
+<style lang="css" scoped></style>
